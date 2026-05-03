e45a4bc8fca5916ec8e1f067b2d7d2aea84369e6
facundovillega231 <facundovillega@proton.me>
2026-05-03 18:59:51 -0300

    Create p2_clip_02_step1b.sql

---
+++ paper2_liquidations/queries/p2_clip_02_step1b.sql
@@ -0,0 +1,11 @@
+-- clip_02 STEP 1b: Keepers activos el 6 de abril de 2025
+SELECT DISTINCT
+    topic2      AS keeper_raw,
+    COUNT(*)    AS n_takes
+FROM ethereum.logs
+WHERE contract_address = 0xc67963a226eddd77b91ad8c421630a1b0adff270
+  AND topic0           = 0x05e309fd6ce72f2ab888a20056bb4210df08daed86f21f95053deb19964d86b1
+  AND block_time >= TIMESTAMP '2025-04-06 00:00:00'
+  AND block_time <  TIMESTAMP '2025-04-07 00:00:00'
+GROUP BY topic2
+ORDER BY n_takes DESC
