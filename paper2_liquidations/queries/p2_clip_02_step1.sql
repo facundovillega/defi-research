9afd46198f8157ed5b13dd7b10ef5413d02a60f1
facundovillega231 <facundovillega@proton.me>
2026-05-03 18:57:46 -0300

    Create p2_clip_02_step1.sql

---
+++ paper2_liquidations/queries/p2_clip_02_step1.sql
@@ -0,0 +1,11 @@
+SELECT
+    topic2      AS keeper_raw,
+    COUNT(*)    AS n_takes
+FROM ethereum.logs
+WHERE contract_address = 0xc67963a226eddd77b91ad8c421630a1b0adff270
+  AND topic0           = 0x05e309fd6ce72f2ab888a20056bb4210df08daed86f21f95053deb19964d86b1
+  AND block_time >= TIMESTAMP '2021-01-01'
+  AND block_time <  TIMESTAMP '2023-01-01'
+GROUP BY topic2
+ORDER BY n_takes DESC
+LIMIT 10
